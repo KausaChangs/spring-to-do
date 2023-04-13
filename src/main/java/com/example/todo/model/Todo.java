cd06e42e2b21b27f1e610033a82cdd8e4cb33caa
KausaChangs <changayakausa@gmail.com>
2023-04-13 11:16:51 +0200

    created Todo and User models

--- src/main/java/com/example/todo/model/Todo.java
+++ src/main/java/com/example/todo/model/Todo.java
@@ -1,4 +1,48 @@
 package com.example.todo.model;
 
+import jakarta.persistence.*;
+import org.springframework.aot.generate.GeneratedTypeReference;
+
+@Entity
+@Table(name="todo")
 public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+
+    @ManyToOne
+    @JoinColumn(name="USER_ID")
+    private User user;
+
+    private String task;
+
+    public Todo(){
+
+    }
+
+    public long getId(){return id;}
+
+    public void setId(long id){this.id = id;}
+
+    public Todo(String task, long id){
+        this.task = task;
+        this.id = id;
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    public void setUser(User user){ this.user = user;}
+
+    public String getTask(){
+        return task;
+    }
+
+    public void setTask(String task){
+        this.task =task;
+    }
+
 }
--- src/main/java/com/example/todo/model/User.java
+++ src/main/java/com/example/todo/model/User.java
@@ -3,8 +3,11 @@ package com.example.todo.model;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
-@Table(name="User")
+@Table(name="Users")
 public class User {
 
     @Id
@@ -16,6 +19,52 @@ public class User {
     private String email;
 
     private String passWord;
+
+    @OneToMany(mappedBy = "user")
+
+    private Set<Todo>  todo = new HashSet<>();
+
+    public Long getId(){ return id;}
+
+    public void setId(Long id){this.id = id;}
+
+    public String getUserName(){
+        return userName;
+    }
+    public void setUserName(String userName){
+       this.userName = userName;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPassWord(){
+        return passWord;
+    }
+
+    public void setPassWord(String passWord){
+        this.passWord = passWord;
+    }
+
+    public Set<Todo> getTodo(){
+        return todo;
+    }
+
+    public void setTodo(Set<Todo> todo){
+        this.todo = todo;
+    }
+
+    @Override
+    public String toString(){
+        return userName;
+    }
+
+
 
 
 }
