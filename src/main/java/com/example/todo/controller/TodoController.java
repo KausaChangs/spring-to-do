e767811f42b32bfa4ac32bf6923e6fe48d3c83f2
KausaChangs <changayakausa@gmail.com>
2023-04-13 10:05:36 +0200

    rendered jsp pages correctly

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -3,11 +3,13 @@ package com.example.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class TodoController {
 
-    @GetMapping("/todd")
+    @RequestMapping("/todo")
     public String getTodos(){
         return "todos";
     }
---
+++ src/main/resources/META-INF/resources/WEB-INF/views/todos.jsp
@@ -0,0 +1,16 @@
+<%@ page import = "jared.simpledatabase.*" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+
+    </head>
+    <body>
+    <h1>Hello World</h1>
+
+
+
+    </body>
+</html>
