4ada2d9285e2edf21cb646da8389ccc4bcef4035
KausaChangs <changayakausa@gmail.com>
2023-04-13 11:39:14 +0200

    created a TodoRepo class

---
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -0,0 +1,21 @@
+package com.example.todo.service;
+
+import com.example.todo.model.Todo;
+import com.example.todo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    TodoRepository todoRepository;
+    public List<Todo> getAllTasks(){
+        return todoRepository.findAll();
+
+    }
+
+
+}
