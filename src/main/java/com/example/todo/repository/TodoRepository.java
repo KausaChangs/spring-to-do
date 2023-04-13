91d218bd1663314708836cdfd378d1682b266190
KausaChangs <changayakausa@gmail.com>
2023-04-13 11:50:28 +0200

    completed TodoService functions

--- src/main/java/com/example/todo/repository/TodoRepository.java
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -3,7 +3,10 @@ package com.example.todo.repository;
 import com.example.todo.model.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
 
+     <Optional>Todo findTodoById(Long id);
 }
--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TodoService {
@@ -15,6 +16,22 @@ public class TodoService {
     public List<Todo> getAllTasks(){
         return todoRepository.findAll();
 
+    }
+
+    public Todo saveTodo(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public Todo getTodoById(Long id){
+        return todoRepository.findTodoById(id);
+    }
+
+    public Todo updateTodo(Todo todo){
+        return todoRepository.save(todo);
+    }
+
+    public void deleteTodo(Todo todo){
+        todoRepository.delete(todo);
     }
 
 
